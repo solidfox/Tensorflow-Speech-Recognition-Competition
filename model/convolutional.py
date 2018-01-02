d2d1f6f624e8ebe46f4d12880dc30290af36730f
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-02 17:24:03 +0100

    WIP Switched to tf.layers
    There are dubious reports of performance issues in keras so might as well use pure tf since we don’t need the cross-compatibility.

--- model/convolutional.py
+++ model/convolutional.py
@@ -6,46 +6,63 @@ from tensorflow.python.keras.layers import Conv2D, Dense, MaxPool2D, Dropout
 
 from data.labels import Label
 
-def convolutional_model(input_layer, seed):
-    model = Sequential()
+def convolutional_model_fn(input_layer, dropout_rate, seed, mode):
+    """
+    Generates a convolutional model.
 
-    model.add(
-        Conv2D(
-            filters=8,
-            kernel_size=(8, 20),
-            activation='relu'))
+    :param input_layer: The inputs to the generated network.
+    :param dropout_rate: The fraction of nodes that are randomly deactivated at each iteration.
+    :param seed: Seed for any random generators.
+    :param mode: A tf.estimator.ModeKeys value indicating if network is to be used for training, evaluation or prediction.
+    :return: The cofigured tensorflow network.
+    """
 
-    model.add(
-        MaxPool2D(
-            pool_size=(8, 8),
-            strides=(8, 8)))
+    previous_layer = input_layer
 
-    model.add(
-        Conv2D(
-            filters=64,
-            kernel_size=(4, 4),
-            activation='relu'))
+    previous_layer = tf.layers.conv2d(
+        inputs=previous_layer,
+        filters=8,
+        kernel_size=[8, 20],
+        padding='same',
+        activation=tf.nn.elu)
 
-    model.add(
-        MaxPool2D(
-            pool_size=(2, 2),
-            strides=(2, 2)))
+    previous_layer = tf.layers.max_pooling2d(
+        inputs=previous_layer,
+        pool_size=[8, 8],
+        strides=[8, 8])
 
-    model.add(
-        Dense(
-            units=200,
-            activation='relu'))
+    previous_layer = tf.layers.conv2d(
+        inputs=previous_layer,
+        filters=64,
+        kernel_size=[4, 4],
+        padding='same',
+        activation=tf.nn.elu)
 
-    model.add(
-        Dropout(
-            rate=0.30,
-            seed=seed))
+    previous_layer = tf.layers.max_pooling2d(
+        inputs=previous_layer,
+        pool_size=[2, 2],
+        strides=[2, 2])
 
-    model.add(
-        tf.keras.layers.S)
+    previous_layer = tf.layers.dense(
+        inputs=previous_layer,
+        units=200,
+        activation=tf.nn.elu)
 
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam
-    )
+    previous_layer = tf.layers.dropout(
+        inputs=previous_layer,
+        rate=dropout_rate,
+        training=mode == tf.estimator.ModeKeys.TRAIN)
 
+    previous_layer = tf.layers.dense(
+        inputs=previous_layer,
+        units=200,
+        activation=tf.nn.softmax_cross_entropy_with_logits)
 
+    previous_layer = tf.layers.dense(
+        inputs=previous_layer,
+        units=Label.n_labels(),
+        activation=tf.nn.elu)
+
+    logits = previous_layer
+
+    if
