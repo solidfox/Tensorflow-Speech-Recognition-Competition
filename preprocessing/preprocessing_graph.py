d722d61f59066aa76e47a55bb5896efb8b808962
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-04 16:16:01 +0100

    Added hyper parameters

--- preprocessing/preprocessing_graph.py
+++ preprocessing/preprocessing_graph.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 from preprocessing.mfcc import tf_mfcc
 
 
-def decoded_samples_preprocessing(decoded_samples):
+def decoded_samples_preprocessing(decoded_samples, num_mel_bins=80, fft_resolution=256):
     mfccs = tf_mfcc(
         decoded_samples,
         window_frame_length=200,
         stride=100,
         lower_edge_hertz=80.0,
         upper_edge_hertz=7600,
-        fft_resolution=256,
-        num_mel_bins=80)
+        fft_resolution=fft_resolution,
+        num_mel_bins=num_mel_bins)
 
     # normalize
     # TODO: check dimension
