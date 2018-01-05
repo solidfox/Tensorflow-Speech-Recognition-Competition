3b099429e141e1b7d98518dcd884cd542a29d901
Kinsark <eric.renmail@gmail.com>
2018-01-05 16:17:09 +0100

    Begin generate_result

--- postprocessing/generate_result.py
+++ postprocessing/generate_result.py
@@ -1,2 +1,22 @@
+from glob import glob
+import os
+import pandas as pd
 
-def generate_result(model):
+def generate_result(model, test_data_dir):
+    # TODO: sub_path
+    sub_path = "./submission/"
+    test_paths = glob(os.path.join(test_data_dir, '*wav'))
+
+    fname, results = [], []
+
+    for path in test_paths:
+        # TODO: Change depending on the model (path or wav), and fill the file
+        predicts = model.predict(path)
+
+        fname.extend(os.path.basename(path=path))
+        results.extend(results)
+
+    df = pd.DataFrame(columns=['fname', 'label'])
+    df['fname'] = fname
+    df['label'] = results
+    df.to_csv(os.path.join(sub_path, 'submission.csv'), index=False)
