32864a5442c2cdf308bac03326200d760ebb28a4
Marc Jourdes <marc.jourdes@etu.unice.fr>
2018-01-06 23:15:39 +0100

    Added tf.summary scalar and histogram in estimator

--- model/estimator_spec.py
+++ model/estimator_spec.py
@@ -12,6 +12,8 @@ def estimator_spec(labels, learning_rate, logits, mode):
 
     with tf.name_scope('Loss'):
         loss = tf.losses.sparse_softmax_cross_entropy(labels, logits)
+        tf.summary.histogram('Histogram', loss)
+        tf.summary.scalar('Loss', loss)
 
     with tf.name_scope('Optimization'):
         if mode == tf.estimator.ModeKeys.TRAIN:
@@ -26,12 +28,26 @@ def estimator_spec(labels, learning_rate, logits, mode):
 
     with tf.name_scope('Evaluation'):
         accuracy = tf.metrics.accuracy(
-                labels=labels,
-                predictions=tf.argmax(inputs=logits, axis=1))
+            labels=labels,
+            predictions=tf.argmax(inputs=logits, axis=1))
         evaluation_metric_operation = {
             'accuracy': accuracy}
-        tf.summary.scalar('Validation_Accuracy', accuracy)
+        tf.summary.scalar('Accuracy', accuracy)
+        tf.summary.histogram('Histogram', accuracy)
         return tf.estimator.EstimatorSpec(
             mode=mode,
             loss=loss,
             eval_metric_ops=evaluation_metric_operation)
+
+
+def variable_summaries(name, var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope('summaries for ' + str(name)):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
