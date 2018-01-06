ac74a7d4f23ae013099d15df40011a406b8acfb5
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-06 02:21:01 +0100

    Worked around noise randomization not being seeded

--- data/tfrecord_validator.py
+++ data/tfrecord_validator.py
@@ -1,19 +1,24 @@
 import tensorflow as tf
+import unittest
+from data import Label
 
 from data import SamplesManager
 
 __author__ = 'Daniel Schlaug'
 
 if __name__ == '__main__':
-    iterator = tf.python_io.tf_record_iterator("train.tfrecord")
+    iterator = tf.python_io.tf_record_iterator("data/train.tfrecord")
     sm = SamplesManager("data")
     i = 0
-    n_samples = 100
+    n_samples = 800
     for example_string in iterator:
         example = tf.train.Example.FromString(example_string)
         if i < n_samples:
+            assert len(sm.files_labels[i].get_wav()) == 16000
+            assert len(example.features.feature.get('wav').float_list.value) == 16000
             assert sm.files_labels[i].label.index == example.features.feature.get('label').int64_list.value[0]
-            assert sm.files_labels[i].get_wav()[0] == example.features.feature.get('wav').float_list.value[0]
+            if sm.files_labels[i].label != Label.silence:
+                assert sm.files_labels[i].get_wav()[0] == example.features.feature.get('wav').float_list.value[0]
         else:
             break
         i = i + 1
