e6a753c78c003bc8ea954b9603bb3d3f4a70576a
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-06 02:21:32 +0100

    Added length to label

--- data/tfrecord_reader.py
+++ data/tfrecord_reader.py
@@ -6,7 +6,7 @@ class TFRecordReader:
         self.validation_set_size = validation_set_size
         self.batch_size = batch_size
         _feature_schema = {
-            'label': tf.FixedLenFeature([], tf.int64),
+            'label': tf.FixedLenFeature([1], tf.int64),
             'wav': tf.FixedLenFeature([16000], tf.float32)
         }
         self.dataset = tf.data.TFRecordDataset([filename]) \
@@ -20,7 +20,8 @@ class TFRecordReader:
         with tf.name_scope('Training_data'):
             training_set = self.dataset.skip(self.validation_set_size) \
                                        .batch(self.batch_size)
-            iterator = training_set.make_initializable_iterator()
+            return training_set
+            # iterator = training_set.make_initializable_iterator()
 
 
 
@@ -28,4 +29,5 @@ class TFRecordReader:
         with tf.name_scope('Validation_data'):
             validation_set = self.dataset.take(self.validation_set_size) \
                                          .batch(self.batch_size)
+            return validation_set
 
