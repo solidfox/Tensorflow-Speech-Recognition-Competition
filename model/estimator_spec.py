6a49f43d1003e7239078f30448911eb5a0a6ed4c
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-06 16:32:56 +0100

    Added scopes

--- model/estimator_spec.py
+++ model/estimator_spec.py
@@ -2,27 +2,35 @@ import tensorflow as tf
 
 __author__ = 'Daniel Schlaug'
 
-def estimator_spec(labels, learning_rate, logits, mode):
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(
-            mode=mode,
-            predictions=tf.nn.softmax(logits, "softmax_predictions"))
-    loss = tf.losses.sparse_softmax_cross_entropy(labels, logits)
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-        train_operation = optimizer.minimize(
-            loss=loss,
-            global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(
-            mode=mode,
-            loss=loss,
-            train_op=train_operation)
 
-    else:  # mode == tf.estimator.ModeKeys.EVAL
-        evaluation_metric_operation = {
-            'accuracy': tf.metrics.accuracy(
+def estimator_spec(labels, learning_rate, logits, mode):
+    with tf.name_scope('Prediction'):
+        if mode == tf.estimator.ModeKeys.PREDICT:
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                predictions=tf.nn.softmax(logits, "softmax_predictions"))
+
+    with tf.name_scope('Loss'):
+        loss = tf.losses.sparse_softmax_cross_entropy(labels, logits)
+
+    with tf.name_scope('Optimization'):
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+            train_operation = optimizer.minimize(
+                loss=loss,
+                global_step=tf.train.get_global_step())
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                loss=loss,
+                train_op=train_operation)
+
+    with tf.name_scope('Evaluation'):
+        accuracy = tf.metrics.accuracy(
                 labels=labels,
-                predictions=tf.argmax(inputs=logits, axis=1))}
+                predictions=tf.argmax(inputs=logits, axis=1))
+        evaluation_metric_operation = {
+            'accuracy': accuracy}
+        tf.summary.scalar('Validation_Accuracy', accuracy)
         return tf.estimator.EstimatorSpec(
             mode=mode,
             loss=loss,
