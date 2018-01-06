48a502d052ed72ad8fc7ab2174ced2b13e528568
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-06 02:22:46 +0100

    Code to test TFReader

--- playground.py
+++ playground.py
@@ -11,14 +11,16 @@ def main():
     print(sample_manager.files_labels[0])
     print(Label.all_labels)
 
-    tfwriter = TFrecord_Writer(sample_manager.files_labels)
-    tfwriter.write()
-    tfreader = TFrecord_reader('train.tfrecords')
-    trainset, valset = tfreader.generate_datasets(1000)
-    print(trainset)
-    print(valset)
+    sample_manager.files_labels[0].get_wav()
+    tfreader = TFRecordReader(filename='data/train.tfrecord', validation_set_size=6000, batch_size=600)
+    first_batch = tfreader.validation_input_fn().make_one_shot_iterator().get_next()
 
-    experiment.hyper_parameter_search.hyper_parameter_search(trainset, valset)
+    with tf.Session() as sess:
+        result = sess.run(first_batch)
+    print(result)
+
+
+    # experiment.hyper_parameter_search.hyper_parameter_search(trainset, valset)
 
     # mfccs = decoded_samples_preprocessing(signals)
     # tf_network = convolutional_model_fn(preprocessed_voice_samples=mfccs,
