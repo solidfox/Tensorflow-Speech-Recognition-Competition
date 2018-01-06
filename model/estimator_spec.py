2e5dc269c39633603875aa7633eec863cea70568
Marc Jourdes <marc.jourdes@etu.unice.fr>
2018-01-06 23:24:06 +0100

    Removed unnecessary function

--- model/estimator_spec.py
+++ model/estimator_spec.py
@@ -38,16 +38,3 @@ def estimator_spec(labels, learning_rate, logits, mode):
             mode=mode,
             loss=loss,
             eval_metric_ops=evaluation_metric_operation)
-
-
-def variable_summaries(name, var):
-    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
-    with tf.name_scope('summaries for ' + str(name)):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev', stddev)
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
-        tf.summary.histogram('histogram', var)
