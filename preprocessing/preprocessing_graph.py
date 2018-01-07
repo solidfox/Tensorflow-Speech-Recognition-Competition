6210bf0b2c0e597b17ceacf4788f1945fffb2e47
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-07 15:04:15 +0100

    Added audio and MFCC image to summary

--- preprocessing/preprocessing_graph.py
+++ preprocessing/preprocessing_graph.py
@@ -4,6 +4,11 @@ from preprocessing.mfcc import tf_mfcc
 
 def decoded_samples_preprocessing(decoded_samples, num_mel_bins=80, fft_resolution=256):
     """Applies mfcc and normalization."""
+    tf.summary.audio(
+        name="Input wav",
+        tensor=decoded_samples,
+        sample_rate=16000
+    )
     mfccs = tf_mfcc(
         decoded_samples,
         window_frame_length=200,
@@ -12,7 +17,10 @@ def decoded_samples_preprocessing(decoded_samples, num_mel_bins=80, fft_resoluti
         upper_edge_hertz=7600,
         fft_resolution=fft_resolution,
         num_mel_bins=num_mel_bins)
-
+    tf.summary.image(
+        name="Raw MFCC",
+        tensor=tf.expand_dims(mfccs, -1)
+    )
     # normalize
     # TODO: check dimension
     normalized_mfcc = tf.nn.l2_normalize(mfccs, dim=[1, 2])
