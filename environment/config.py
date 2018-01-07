4ace20f64800d71a0c95beaba09478c01f2d3e72
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-07 14:55:06 +0100

    Change output_dir on hops

--- environment/config.py
+++ environment/config.py
@@ -1,6 +1,12 @@
+import environment
+
 __author__ = 'Daniel Schlaug'
 
 
 class EnvironmentConfig:
     def __init__(self):
-        self.model_output_dir = 'model_output'
+        if environment.running_on_hops:
+            import hops
+            self.model_output_dir = hops.tensorboard.logdir()
+        else:
+            self.model_output_dir = 'model_output'
