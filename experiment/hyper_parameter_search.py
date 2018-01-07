504175efa5acf8c84df9473747966fdfe183faf9
Unknown <marc.jourdes@etu.unice.fr>
2018-01-07 11:19:10 +0100

    Wrapped hyper_parameter_search with a default graph

--- experiment/hyper_parameter_search.py
+++ experiment/hyper_parameter_search.py
@@ -11,62 +11,58 @@ def hyper_parameter_search():
     Configures and runs the hyper-parameter search
 
     """
-    # Set up the environment
-    env_conf = environment.EnvironmentConfig()
-    dataset = data.TFRecordReader(
-        filename='data/train.tfrecord',
-        validation_set_size=6000,
-        batch_size=100
-    )
-    train_input_fn = dataset.next_training_batch
-    eval_input_fn = dataset.next_validation_batch
+    g = tf.Graph()
+    with g.as_default():
+        # Set up the environment
+        env_conf = environment.EnvironmentConfig()
+        dataset = data.TFRecordReader(
+            filename='data/train.tfrecord',
+            validation_set_size=6000,
+            batch_size=32
+        )
+        train_input_fn = dataset.next_training_batch
+        eval_input_fn = dataset.next_validation_batch
 
-    run_config = tf.contrib.learn.RunConfig(
-        model_dir=env_conf.model_output_dir,
-        save_summary_steps=100
-        # save_checkpoints_steps=
-    )
+        run_config = tf.contrib.learn.RunConfig(
+            model_dir=env_conf.model_output_dir,
+            save_summary_steps=100
+            # save_checkpoints_steps=
+        )
 
-    # For Tensorboard
-    summary_hook = tf.train.SummarySaverHook(
-        save_steps=run_config.save_summary_steps,
-        scaffold=tf.train.Scaffold(),
-        summary_op=tf.summary.merge_all())
+        # For Tensorboard
+        summary_hook = tf.train.SummarySaverHook(
+            save_steps=run_config.save_summary_steps,
+            scaffold=tf.train.Scaffold(),
+            summary_op=tf.summary.merge_all())
 
-    experiment_factory = ExperimentFactory(
-        environment_config=env_conf,
-        train_input_fn=train_input_fn,
-        eval_input_fn=eval_input_fn,
-        eval_hooks=[summary_hook]
-    )
+        experiment_factory = ExperimentFactory(
+            environment_config=env_conf,
+            train_input_fn=train_input_fn,
+            eval_input_fn=eval_input_fn,
+            eval_hooks=[summary_hook]
+        )
 
-    # Create hyper parameter grids for each experiment.
+        # Create hyper parameter grids for each experiment.
 
-    tf_h_params = tf.contrib.training.HParams(
-        learning_rate=0.001,
-        dropout_rate=0.3,
-        random_seed=2222,
-        training_steps=3000
-    )
+        tf_h_params = tf.contrib.training.HParams(
+            learning_rate=0.001,
+            dropout_rate=0.3,
+            random_seed=2222,
+            training_steps=3000
+        )
 
-    # Run experiments with all their corresponding parameter grids.
-    # This is where TFLauncher comes in on hops
+        # Run experiments with all their corresponding parameter grids.
+        # This is where TFLauncher comes in on hops
 
-    tf.contrib.learn.learn_runner.run(
-        experiment_fn=experiment_factory.convolutional_experiment,
-        run_config=run_config,
-        schedule="train_and_evaluate",
-        hparams=tf_h_params
-    )
+        tf.contrib.learn.learn_runner.run(
+            experiment_fn=experiment_factory.convolutional_experiment,
+            run_config=run_config,
+            schedule="train_and_evaluate",
+            hparams=tf_h_params
+        )
 
-    # (Evaluate the results and spin off more experiments?)
+        # (Evaluate the results and spin off more experiments?)
 
-
-def input_fn(dataset):
-    iterator = dataset.make_one_shot_iterator()
-    wav, label = iterator.get_next()
-
-    return wav, label
 
 if __name__ == '__main__':
     hyper_parameter_search()
--- playground.py
+++ playground.py
@@ -1,6 +1,5 @@
 from data import *
 import experiment.hyper_parameter_search
-
 signals = tf.placeholder(tf.float32, [None, 16000])
 
 
@@ -13,14 +12,16 @@ def main():
 
     sample_manager.files_labels[0].get_wav()
     tfreader = TFRecordReader(filename='data/train.tfrecord', validation_set_size=6000, batch_size=600)
-    wavs, labels = tfreader.validation_input_fn().make_one_shot_iterator().get_next()
+    wavs, labels = tfreader.next_training_batch()
 
-    with tf.Session() as sess:
-        result = sess.run(wavs)
-    print(result)
-    with tf.Session() as sess:
-        result = sess.run(wavs)
-    print(result)
+    experiment.hyper_parameter_search.hyper_parameter_search()
+
+    # with tf.Session() as sess:
+    #     result = sess.run(wavs)
+    # print(result)
+    # with tf.Session() as sess:
+    #     result = sess.run(wavs)
+    # print(result)
 
     # experiment.hyper_parameter_search.hyper_parameter_search(trainset, valset)
 
