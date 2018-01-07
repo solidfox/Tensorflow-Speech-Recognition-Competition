f42c193fc680d81130e16a4076fe2ab6c6d448ab
Kinsark <eric.renmail@gmail.com>
2018-01-07 22:50:18 +0100

    Still need to see how to handle estimator and to create the right input_fn

--- postprocessing/generate_result.py
+++ postprocessing/generate_result.py
@@ -1,22 +1,46 @@
 from glob import glob
 import os
 import pandas as pd
+from scipy.io import wavfile
+import tensorflow as tf
+import numpy as np
 
-def generate_result(model, test_data_dir):
-    # TODO: sub_path
-    sub_path = "./submission/"
-    test_paths = glob(os.path.join(test_data_dir, '*wav'))
 
-    fname, results = [], []
+def generate_result(estimator):
+    sub_path = "submission/"
+    test_data_dir = "data/test/"
+    test_paths = glob(os.path.join(test_data_dir, '[!_]*/*wav'))
 
+    fname, results, set = [], [], []
+
+    # TODO: create wavs Tensors
     for path in test_paths:
-        # TODO: Change depending on the model (path or wav), and fill the file
-        predicts = model.predict(path)
+        sample_rate, samples = wavfile.read(path)
+        set.append(samples)
 
-        fname.extend(os.path.basename(path=path))
-        results.extend(results)
+        # fname.extend(os.path.basename(path=path))
+        # fname.extend(path)
+        # results.extend(results)
 
+    # predictions = estimator.predict(
+    #     input_fn=test_set,
+    #     predict_keys=None,
+    #     hooks=None,
+    #     checkpoint_path=None
+    # )
+
+    # Generator
+    # for i in predictions:
+    #     print(predictions)
+    # Do the prediction
+
+
+    # Create the submission file
     df = pd.DataFrame(columns=['fname', 'label'])
     df['fname'] = fname
     df['label'] = results
     df.to_csv(os.path.join(sub_path, 'submission.csv'), index=False)
+
+
+# if __name__ == '__main__':
+#     generate_result()
