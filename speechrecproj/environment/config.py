1e64e76d5257c7f0814b1d9c05d6697c257d41cf
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-08 19:21:40 +0100

    Correct path to hops dataset

--- speechrecproj/environment/config.py
+++ speechrecproj/environment/config.py
@@ -8,7 +8,7 @@ class EnvironmentConfig:
         if environment.running_on_hops:
             import hops
             self.output_dir = hops.tensorboard.logdir()
-            self.input_dir = 'hdfs:///Projects/Google_Brain_Speech_Recognition_Contest/Tensorflow_Speech_Recognition_Challenge'
+            self.input_dir = 'hdfs:///Projects/TF_Speech_Project/Tensorflow_Speech_Recognition'
         else:
             self.output_dir = 'output'
             self.input_dir = 'input'
