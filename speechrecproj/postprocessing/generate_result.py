ce321e7ce64d6482cc52a51e72a44a8b4e45ff47
Kinsark <eric.renmail@gmail.com>
2018-01-09 13:24:52 +0100

    Added generator

--- speechrecproj/postprocessing/generate_result.py
+++ speechrecproj/postprocessing/generate_result.py
@@ -4,7 +4,7 @@ import pandas as pd
 from scipy.io import wavfile
 import tensorflow as tf
 import numpy as np
-from data.labels import Label
+from speechrecproj.data.labels import Label
 
 sub_path = "submission/"
 test_data_dir = "data/test/"
@@ -16,7 +16,7 @@ def generate_result(estimator):
     for path in test_paths:
         _, sample = wavfile.read(path)
         fname.append(os.path.basename(path))
-        samps.append(sample)
+        # samps.append(sample)
 
     predictions = predict(estimator=estimator)
 
@@ -32,10 +32,22 @@ def generate_result(estimator):
     df.to_csv(os.path.join(sub_path, 'submission.csv'), index=False)
 
 def test_input():
-    samples = tf.constant(np.array(samps), dtype=tf.float32)
-    dataset = tf.contrib.data.Dataset.from_tensor_slices((samples,))
+    # samples = tf.constant(np.array(samps), dtype=tf.float32)
+    # dataset = tf.contrib.data.Dataset.from_tensor_slices((samples,))
+    dataset = tf.contrib.data.Dataset.from_generator(
+        generator=test_data_generator(),
+        output_types=tf.float32,
+        output_shapes=tf.TensorShape([16000])
+    )
     return dataset.batch(1).make_one_shot_iterator().get_next()
 
+def test_data_generator():
+    def generator():
+        for path in test_paths:
+            _, wav = wavfile.read(path)
+            wav = wav.astype(np.float32)
+            yield wav
+    return generator
 
 def predict(estimator):
     return estimator.predict(
