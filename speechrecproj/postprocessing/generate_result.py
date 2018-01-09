ceebe459ce692756985d819b78b6ea623d50f943
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-09 23:19:59 +0100

    Cleaned up generate_result

--- speechrecproj/postprocessing/generate_result.py
+++ speechrecproj/postprocessing/generate_result.py
@@ -5,41 +5,43 @@ from scipy.io import wavfile
 import tensorflow as tf
 import numpy as np
 from speechrecproj.data.labels import Label
-
-sub_path = "submission/"
-test_data_dir = "data/test/"
-test_paths = glob(os.path.join(test_data_dir, '[!_]*/*wav'))
-fname, labels, samps = [], [], []
+from os.path import dirname, basename
 
 
-def generate_result(estimator):
+def generate_result(estimator, test_data_dir, submission_filename='submission/submission.csv'):
+    test_paths = glob(os.path.join(test_data_dir, '[!_]*/*wav'))
+
+    if not os.path.exists(dirname(submission_filename)):
+        raise AttributeError("The folder {} does not exists. Create it and run again.".format(dirname(submission_filename)))
+
+    if os.path.exists(submission_filename):
+        raise AttributeError("{} already exists. Delete it and run again.".format(submission_filename))
+
+    fname, labels = [], []
+
     for path in test_paths:
-        _, sample = wavfile.read(path)
         fname.append(os.path.basename(path))
-        # samps.append(sample)
 
     predictions = predict(estimator=estimator)
 
-    for p in predictions:
-        print(Label.from_index(p['label']).string)
+    for i, p in enumerate(predictions):
+        print(i)
         labels.append(Label.from_index(p['label']).string)
-
 
     # Create the submission file
     df = pd.DataFrame(columns=['fname', 'label'])
     df['fname'] = fname
     df['label'] = labels
-    df.to_csv(os.path.join(sub_path, 'submission.csv'), index=False)
+    df.to_csv(submission_filename, index=False)
+
 
 def test_input():
-    # samples = tf.constant(np.array(samps), dtype=tf.float32)
-    # dataset = tf.contrib.data.Dataset.from_tensor_slices((samples,))
     dataset = tf.contrib.data.Dataset.from_generator(
         generator=test_data_generator(),
         output_types=tf.float32,
         output_shapes=tf.TensorShape([16000])
     )
-    return dataset.batch(1).make_one_shot_iterator().get_next()
+    return dataset.batch(128).make_one_shot_iterator().get_next()
 
 def test_data_generator():
     def generator():
