b5c55f1e4fec1e83c878a7940d4eddc6aaa22a4e
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-12 17:20:01 +0100

    Fixed environment

--- speechrecproj/environment/config.py
+++ speechrecproj/environment/config.py
@@ -1,14 +1,14 @@
-# from speechrecproj import environment
+from speechrecproj.environment import running_on_hops
 
 __author__ = 'Daniel Schlaug'
 
 
 class EnvironmentConfig:
     def __init__(self):
-        # if environment.running_on_hops:
-        #     import hops
-        #     self.output_dir = hops.tensorboard.logdir()
-        #     self.input_dir = 'hdfs:///Projects/TF_Speech_Project/Tensorflow_Speech_Recognition'
-        # else:
-        self.output_dir = 'output'
-        self.input_dir = 'input'
+        if running_on_hops:
+            import hops
+            self.output_dir = hops.tensorboard.logdir()
+            self.input_dir = 'hdfs:///Projects/TF_Speech_Project/Tensorflow_Speech_Recognition'
+        else:
+            self.output_dir = 'output'
+            self.input_dir = 'input'
