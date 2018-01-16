65635a67209e495ec3753bd26900c657c2891acd
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-16 09:22:46 +0100

    WIP Use SavedModel

--- postprocessing/generate_result.py
+++ postprocessing/generate_result.py
@@ -1,49 +1,60 @@
-from glob import glob
 import os
-import pandas as pd
-from scipy.io import wavfile
-import tensorflow as tf
-import numpy as np
-from speechrecproj.data.labels import Label
 from os.path import dirname
+from glob import glob
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from scipy.io import wavfile
+from tensorflow.contrib.predictor import from_saved_model
+
+from speechrecproj.environment.config import EnvironmentConfig
+from speechrecproj.data.labels import Label
 
 
-def generate_result(estimator, test_data_dir, submission_filename='submission/submission.csv'):
-    test_paths = glob(os.path.join(test_data_dir, '[!_]*/*wav'))
+def generate_result(test_data_dir, submission_filename='submission/submission.csv'):
+    env = EnvironmentConfig()
 
+    submission_filename = os.path.join(env.output_dir, 'submission.csv')
     if not os.path.exists(dirname(submission_filename)):
         raise AttributeError("The folder {} does not exists. Create it and run again.".format(dirname(submission_filename)))
-
     if os.path.exists(submission_filename):
         raise AttributeError("{} already exists. Delete it and run again.".format(submission_filename))
 
-    fname, labels = [], []
+    test_paths = glob(os.path.join(test_data_dir, '[!_]*/*wav'))
+    generator = test_data_generator(test_paths)
 
-    for path in test_paths:
-        fname.append(os.path.basename(path))
+    predictor = from_saved_model(
+        export_dir=os.path.join(env.output_dir, '1516090613')
+    )
 
-    predictions = predict(estimator=estimator, test_paths=test_paths)
+    labels = []
 
-    for i, p in enumerate(predictions):
-        print(i)
-        labels.append(Label.from_index(p['label']).string)
+    for i, wavs in enumerate(generator()):
+        if i % 100 == 0:
+            print(i)
+        dictionary = dict(
+            wav=wavs
+        )
+        prediction = predictor(dictionary)
+        labels.append(Label.from_index(prediction['label']).string)
 
     # Create the submission file
     df = pd.DataFrame(columns=['fname', 'label'])
-    df['fname'] = fname
+    df['fname'] = map(os.path.basename, test_paths)
     df['label'] = labels
     df.to_csv(submission_filename, index=False)
 
 
-def test_input(test_paths):
-    def intpu_fn():
-        dataset = tf.contrib.data.Dataset.from_generator(
-            generator=test_data_generator(test_paths),
-            output_types=tf.float32,
-            output_shapes=tf.TensorShape([16000])
-        )
-        return dataset.batch(128).make_one_shot_iterator().get_next()
-    return intpu_fn
+# def test_input(test_paths):
+#     def input_fn():
+#         dataset = tf.contrib.data.Dataset.from_generator(
+#             generator=test_data_generator(test_paths),
+#             output_types=tf.float32,
+#             output_shapes=tf.TensorShape([16000])
+#         )
+#         return dataset.batch(128).make_one_shot_iterator().get_next()
+#     return input_fn
 
 
 def test_data_generator(test_paths):
@@ -54,11 +65,15 @@ def test_data_generator(test_paths):
             yield wav
     return generator
 
+#
+# def predict(estimator, test_paths):
+#     return estimator.predict(
+#         input_fn=test_input(test_paths),
+#         predict_keys=None,
+#         hooks=None,
+#         checkpoint_path=None
+#     )
 
-def predict(estimator, test_paths):
-    return estimator.predict(
-        input_fn=test_input(test_paths),
-        predict_keys=None,
-        hooks=None,
-        checkpoint_path=None
-    )
+if __name__ == '__main__':
+    env = EnvironmentConfig()
+    generate_result(test_data_dir=os.path.join(env.input_dir, "test"))
