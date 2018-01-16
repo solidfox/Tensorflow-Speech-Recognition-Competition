1b31a304c7409e51808d2706d28b56235844e69c
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-01-16 09:21:53 +0100

    Fixed environment package

--- speechrecproj/environment/__init__.py
+++ speechrecproj/environment/__init__.py
@@ -1,8 +1,4 @@
 from config import EnvironmentConfig
-import pkgutil
-
 
 __author__ = 'Daniel Schlaug'
-
-running_on_hops = pkgutil.find_loader('hops') is not None
 
--- speechrecproj/environment/config.py
+++ speechrecproj/environment/config.py
@@ -1,14 +1,18 @@
-from speechrecproj.environment import running_on_hops
+import pkgutil
 
 __author__ = 'Daniel Schlaug'
 
 
 class EnvironmentConfig:
     def __init__(self):
-        if running_on_hops:
+        if self.running_on_hops:
             import hops
             self.output_dir = hops.tensorboard.logdir()
             self.input_dir = 'hdfs:///Projects/TF_Speech_Project/Tensorflow_Speech_Recognition'
         else:
             self.output_dir = 'output'
             self.input_dir = 'input'
+
+    @property
+    def running_on_hops(self):
+        return pkgutil.find_loader('hops') is not None
